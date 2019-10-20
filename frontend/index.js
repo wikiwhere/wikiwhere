e7f46ed06b6fa23a1f580808aca2df6e4a7f2b5d
David Chen <chendavid2012@gmail.com>
2019-10-19 22:42:41 -0400

    connect frontend to backend and allow double click behavior

--- frontend/index.js
+++ frontend/index.js
@@ -1,86 +1,132 @@
+let labelOffsetX = 6;
+let labelOffsetY = 3;
+let circleRadius = 5;
+
 let width = 960,
-    height = 500;
+    height = 600;
 
 let scale = d3.scaleOrdinal(d3.schemeCategory10);
+let color = (d) => scale(d.group);
 
-let labelOffsetX = 6,
-    labelOffsetY = 3,
-    circleRadius = 5;
-
-let data = {
-    nodes: [
-        {id: "Article 1", group: 1},
-        {id: "Article 2", group: 2},
-    ],
-    links: [
-        {source: "Article 1", target: "Article 2"},
-    ]
-};
-
-let svg = d3.select("svg")
-    .attr("viewBox", [-width / 2, -height / 2, width, height]);
-
-let nodes = [];
 let links = [];
+let nodes = [];
+let nodeSet = new Set([]);
 
 let simulation = d3.forceSimulation(nodes)
-    .force("link", d3.forceLink(links).id(d => d.id).distance(0).strength(1))
-    .force("charge", d3.forceManyBody().strength(-50))
+    .force("link", d3.forceLink(links).id(d => d.id).distance(0).strength(0.5))
+    .force("charge", d3.forceManyBody().strength(-500))
     .force("x", d3.forceX())
     .force("y", d3.forceY());
 
-let link = svg.append("g")
-        .attr("class","links")
-        .selectAll("line"),
-    node = svg.append("g")
-        .attr("class", "nodes")
-        .selectAll("g");
+let resolveDiff = (newNodes) => {
+  newNodes = newNodes.filter((node) => !nodeSet.has(node.id));
+  newNodes.forEach((node) => nodeSet.add(node.id));
+  return newNodes;
+}
 
-let search = () => {
-    let newNodes = [
-        {
-            id: "Article 3",
-            group: 3
-        },
-        {
-            id: "Article 4",
-            group: 4
-        }];
-    let newLinks = [
-        {source: "Article 4", target: "Article 3"},
-    ];
+let restart = () => {
+  // Draw new nodes & links
+  link = d3.select(".links")
+      .selectAll("line")
+    .data(links)
+    .enter().insert("line")
+    .attr("stroke", "black")
+    .attr("stroke-width", 1);
 
-    nodes = [...nodes, ...newNodes.map(d => Object.create(d))];
-    links = [...links, ...newLinks.map(d => Object.create(d))];
+  node = d3.select(".nodes")
+      .selectAll("g")
+      .data(nodes)
+      .enter().append("g")
+      .on("dblclick", (d) => { search(d.id, d.group, false) });
 
-    restart();
+  circles = node.insert("circle")
+    .attr("r", circleRadius)
+    .attr("fill", color)
+    .call(drag(simulation));
+
+  node = d3.select(".nodes")
+    .selectAll("g");
+
+  node.selectAll("text").remove();
+  node.append("title")
+    .text((d) => {
+        return d.id;
+    });
+
+  labels = node.append("text")
+    .text((d) => {
+        return d.id;
+    })
+    .attr("x", labelOffsetX)
+    .attr("y", labelOffsetY);
+
+  console.log(node);
+  
+  // Update simulation parameters
+  simulation.nodes(nodes);
+  simulation.force("link").links(links);
+  simulation.alpha(1).restart();
+}
+
+async function search (article, depth, shouldReset, article2) {
+  // Param: 1 is reset, 0 is update
+  // Get and set new nodes & links
+  const Http = new XMLHttpRequest();
+  const childUrl=`https://wikiwhere.org/api/children?source=${article}&group=${depth}`;
+  const pathUrl=`https://wikiwhere.org/api/path?source=${article}&target=${article2}`;
+  const url = article2 ? pathUrl : childUrl;
+  
+  let newNodes, newLinks;
+  
+  const response = await fetch(url);
+  const data = await response.json();
+  newNodes = resolveDiff(data.nodes);
+  newLinks = data.links;
+  console.log(newNodes);
+  console.log(newLinks);
+//  newNodes = [
+//    { id: "Article 1", group: 1 },
+//    { id: "Article 2", group: 2 },
+//    { id: "Article 3", group: 3 },
+//    { id: "Article 4", group: 4 },
+//  ];
+//  newLinks = [
+//    { source: "Article 1", target: "Article 2" },
+//    { source: "Article 3", target: "Article 2" },
+//    { source: "Article 4", target: "Article 1" },
+//  ];
+
+  nodes = shouldReset ? newNodes : [...nodes, ...newNodes];
+  links = shouldReset ? newLinks : [...links, ...newLinks];
+
+  restart();
 };
 
 let ticked = () => {
-    link = d3.select(".links")
-        .selectAll("line");
+  node = d3.select(".nodes")
+      .selectAll("g");
 
-    node = d3.select(".nodes")
-        .selectAll("g");
+  link = d3.select(".links")
+      .selectAll("line");
 
-    link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
+  link
+      .attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y);
 
-    node
-        .select("circle")
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
+  node
+      .select("circle")
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y);
 
-    node
-        .select("text")
-        .attr("x", d => d.x + labelOffsetX)
-        .attr("y", d => d.y + labelOffsetY);
+  node
+      .select("text")
+      .attr("x", d => d.x + labelOffsetX)
+      .attr("y", d => d.y + labelOffsetY);
 };
 
-let drag = simulation => {
+drag = simulation => {
 
     let dragStarted = (d) => {
         if (!d3.event.active) {
@@ -99,8 +145,8 @@ let drag = simulation => {
         if (!d3.event.active) {
             simulation.alphaTarget(0);
         }
-        d.fx = null;
-        d.fy = null;
+        d.fx = undefined;
+        d.fy = undefined;
     };
 
     return d3.drag()
@@ -109,41 +155,43 @@ let drag = simulation => {
         .on("end", dragEnded);
 };
 
-let color = (d) => scale(d.group);
+let svg = d3.select("svg")
+            .attr("viewBox", [-width / 2, -height / 2, width, height]);
 
-let restart = () => {
-    link = link.data(links);
+let link = svg.append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(links)
+    .enter().append("line")
+    .attr("stroke", "black")
+    .attr("stroke-width", 1);
 
-    link.enter().append("line")
-        .attr("stroke", "black")
-        .attr("stroke-width", 1);
+let node = svg.append("g")
+    .attr("class", "nodes")
+    .selectAll("g")
+    .data(nodes)
+    .enter().append("g")
+    .on("click", (e) => { console.log("click", e)});
 
-    node = node.data(nodes)
-        .enter().append("g");
+let circles = node.append("circle")
+    .attr("r", circleRadius)
+    .attr("fill", color)
+    .call(drag(simulation))
 
-    let circles = node.append("circle")
-        .attr("r", circleRadius)
-        .attr("fill", color)
-        .call(drag(simulation));
+let labels = node.append("text")
+    .text((d) => {
+        return d.id;
+    })
+    .attr("x", labelOffsetX)
+    .attr("y", labelOffsetY);
 
-    let labels = node.append("text")
-        .text((d) => {
-            return d.id;
-        })
-        .attr("x", labelOffsetX)
-        .attr("y", labelOffsetY);
+node.append("title")
+    .text((d) => {
+        return d.id;
+    });
+simulation
+    .nodes(nodes)
+    .on("tick", ticked);
 
-    node.append("title")
-        .text((d) => {
-            return d.id;
-        });
-
-    simulation
-        .nodes(nodes)
-        .on("tick", ticked);
-
-    simulation.force("link")
-        .links(links);
-};
-
-restart();
+simulation.force("link")
+    .links(links);
